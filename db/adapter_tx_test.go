20132a379a54de22aecd100dba98972823cbb34b
insomnius <aliana.arib@gmail.com>
2021-02-09 23:20:30 +0700

    Full coverage transaction adapter

--- db/adapter_tx_test.go
+++ db/adapter_tx_test.go
@@ -202,4 +202,56 @@ func TestTx(t *testing.T) {
 			assert.Nil(t, mockDB.ExpectationsWereMet())
 		})
 	})
+
+	t.Run("ExecContext", func(t *testing.T) {
+		t.Run("When query execution is done it will return results", func(t *testing.T) {
+			sqldb, mockDB, err := sqlmock.New(sqlmock.MonitorPingsOption(true))
+			if err != nil {
+				t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+			}
+			defer sqldb.Close()
+
+			mockDB.ExpectBegin()
+			mockDB.ExpectExec(`insert into users \(1\) values\(id\)`).WillReturnResult(sqlmock.NewResult(1, 1))
+			mockDB.ExpectCommit()
+
+			sql := db.Adapt(sqldb)
+			err = sql.Transaction(ktx, "transaction-test", func(tx db.TX) exception.Exception {
+				result, err := tx.ExecContext(ktx, "test-query-1", "insert into users (1) values(id)")
+				lastID, err := result.LastInsertId()
+				assert.Equal(t, int64(1), lastID)
+				assert.Nil(t, err)
+
+				rowsAffected, err := result.RowsAffected()
+				assert.Equal(t, int64(1), rowsAffected)
+				assert.Nil(t, err)
+
+				return err
+			})
+
+			assert.Nil(t, err)
+			assert.Nil(t, mockDB.ExpectationsWereMet())
+		})
+
+		t.Run("When query execution error then it will return error", func(t *testing.T) {
+			sqldb, mockDB, err := sqlmock.New(sqlmock.MonitorPingsOption(true))
+			if err != nil {
+				t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+			}
+			defer sqldb.Close()
+
+			mockDB.ExpectBegin()
+			mockDB.ExpectExec(`insert into users \(1\) values\(id\)`).WillReturnError(errors.New("unexpected error"))
+			mockDB.ExpectRollback()
+
+			sql := db.Adapt(sqldb)
+			err = sql.Transaction(ktx, "transaction-test", func(tx db.TX) exception.Exception {
+				_, err := tx.ExecContext(ktx, "test-query-1", "insert into users (1) values(id)")
+				return err
+			})
+
+			assert.NotNil(t, err)
+			assert.Nil(t, mockDB.ExpectationsWereMet())
+		})
+	})
 }
