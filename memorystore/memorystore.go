0669c81eecb817d3670ebeb232cd9e6827917e76
insomnius <aliana.arib@gmail.com>
2021-02-09 23:56:44 +0700

    Redefining cache as memorystore

--- memorystore/memorystore.go
+++ memorystore/memorystore.go
@@ -1,4 +1,4 @@
-package cache
+package memorystore
 
 import (
 	"time"
@@ -6,14 +6,14 @@ import (
 	"github.com/codefluence-x/monorepo/kontext"
 )
 
-// Cache is interface to connect to cache infrastructure
-type Cache interface {
+// MemoryStore is interface to connect to cache infrastructure
+type MemoryStore interface {
 	// To make it not expire set expiration into 0
 	Set(ctx kontext.Context, key string, value []byte, expiration time.Duration) error
 	Get(ctx kontext.Context, key string) (Item, error)
 }
 
-// Item contain result get from Cache interface
+// Item contain result got from MemoryStore interface
 type Item interface {
 	Key() string
 	Value() []byte
