76442652138012cf89ee05caab894b3e2298efc4
Insomnius <aliana.arib@gmail.com>
2022-09-04 20:14:32 +0700

    Create WithErrors method in jsonapi package

--- jsonapi/jsonapi_test.go
+++ jsonapi/jsonapi_test.go
@@ -45,6 +45,30 @@ func TestJSONAPI(t *testing.T) {
 		assert.Equal(t, "JSONAPI Error:\n[title] Detail: detail, Code: ERR401\n[title_2] Detail: detail_2, Code: ERR401\n", response.Errors.Error())
 	})
 
+	t.Run("Nil data won't print in json format using WithErrors option", func(t *testing.T) {
+		response := jsonapi.BuildResponse(
+			jsonapi.WithException("ERR401", http.StatusNotFound, exception.Throw(
+				errors.New("unexpected error"),
+				exception.WithDetail("detail"),
+				exception.WithTitle("title"),
+			)),
+			jsonapi.WithExceptionMeta("ERR401", http.StatusNotFound, exception.Throw(
+				errors.New("unexpected error"),
+				exception.WithDetail("detail_2"),
+				exception.WithTitle("title_2"),
+			), jsonapi.Meta{
+				"sample": "sample",
+			}),
+		)
+
+		response2 := jsonapi.BuildResponse(jsonapi.WithErrors(response.Errors))
+
+		b, _ := json.Marshal(response2)
+		assert.Equal(t, "{\"errors\":[{\"title\":\"title\",\"detail\":\"detail\",\"code\":\"ERR401\",\"status\":404},{\"title\":\"title_2\",\"detail\":\"detail_2\",\"code\":\"ERR401\",\"status\":404,\"meta\":{\"sample\":\"sample\"}}]}", string(b))
+		assert.Equal(t, http.StatusNotFound, response2.HTTPStatus())
+		assert.Equal(t, "JSONAPI Error:\n[title] Detail: detail, Code: ERR401\n[title_2] Detail: detail_2, Code: ERR401\n", response2.Errors.Error())
+	})
+
 	t.Run("500 http status when error status not set", func(t *testing.T) {
 		response := jsonapi.BuildResponse(
 			jsonapi.WithException("ERR401", 0, exception.Throw(
